5a7006a3070c39784ddc8a58062b482b24840ad7
Raissa Vieira <raissavieira125@gmail.com>
2021-02-02 17:14:09 -0300

    test: Adicionando assercoes ao teste
    Validacao se o produto foi add ao carrinho, se o endereco de entrega eh
    o mesmo de cobranca, se a compra foi finalizada com sucesso,  se a
    compra foi executado por meio do ID

--- cypress/integration/compra.spec.js
+++ cypress/integration/compra.spec.js
@@ -16,6 +16,15 @@ context('Compra', () => {
             .first() // o elemento que desejamos era o primeiro 
             .click();
 
+        // Validando se o produto foi add ao carrinho com sucesso
+        
+        cy.get('.icon-ok')
+            .parent()
+            .should('contain.text', 'Product successfully added to your shopping cart');
+
+        cy.get('span#layer_cart_product_title')
+            .should('contain.text', nomeProduto);
+
         cy.get(".button-container a[href$='controller=order']").click();
         
         cy.get(".cart_navigation a[href$='controller=order&step=1']").click();
@@ -25,7 +34,14 @@ context('Compra', () => {
 
         cy.get('button#SubmitLogin').click();
 
+        // Validadndo se o endereco de entrega eh igual o de cobranca
         // [type=checkbox]#addressesAreEquals
+        // tipo de assercao, atributo, valor
+
+        cy.get('[type=checkbox]#addressesAreEquals')
+            .should('have.attr', 'checked', 'checked');
+        cy.get('[type=checkbox]#addressesAreEquals')
+            .should('have.attr', 'name', 'same');
 
         cy.get('button[name=processAddress]').click();
         
@@ -44,6 +60,28 @@ context('Compra', () => {
 
         cy.get('.cheque-indent strong')
             .should('contain.text', 'Your order on My Store is complete.');
+
+        cy.get('div.box')
+            .invoke('text')
+            .then((text) => {
+                console.log(text);
+
+                // Captura do ID do pedido
+                console.log(text.match(/[A-Z][A-Z]+/g)[1]);
+
+                // Escrita de um arquivo json com o conteudo do pedido
+                // caminho do arquivo e o conteudo do arquivo
+                cy.writeFile('cypress/fixtures/pedido.json', { id: `${text.match(/[A-Z][A-Z]+/g)[1]}`})
+            });
+        
+        cy.get('a.button-exclusive').click();
+
+        // Leitura de arquivo
+
+        cy.readFile('cypress/fixtures/pedido.json').then(pedido => {
+            
+            cy.get('tr.first_item .history_link a').should('contain.text', pedido.id);
+        })
 
     });
 });
